757468ecf67b99cd930aa785a4b8b5242a3f235d
Sebastian Possos <sebastian.possos@intel.com>
2018-09-16 16:13:13 -0700

    [MCTF/ASC] CMC update for all kernels and general cleanup
    Migration of Cm kernel compilation from icl to cmc for
    all platforms, addition of compiled kernel for ICL and
    ICLLP (Gen11).
    Fixed issues related to low power platforms (BXT, GLK, ICLLP)
    based on constants being interpreted by compiler as doubles
    which are not supported on those platforms.
    General cleanup of MCTF code, removal of MFX_ENABLE_MCTF_EXT
    definition from MCTF and sample_vpp code as mctf extended
    features are not approved until further notice.
    Removal of deprecated feature MRE and cleanup.
    
    Issue: -
    Test: Manual test

--- _studio/mfx_lib/genx/mctf/include/genx_sd_icllp_isa.h
+++ _studio/mfx_lib/genx/mctf/include/genx_sd_icllp_isa.h
@@ -5,10 +5,9 @@
 // nondisclosure agreement with Intel Corporation and may not be copied
 // or disclosed except in accordance with the terms of that agreement.
 //
-// Copyright(C) 2012-2018 Intel Corporation. All Rights Reserved.
+// Copyright(c) 2012-2018 Intel Corporation. All Rights Reserved.
 //
-
-#ifndef __genx_scd_icllp__
-#define __genx_scd_icllp__
-extern const unsigned char genx_scd_icllp[19193];
+#ifndef __genx_sd_icllp__
+#define __genx_sd_icllp__
+extern const unsigned char genx_sd_icllp[103162];
 #endif
